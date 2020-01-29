235a32c4259821060535e5a4ee759beb5179826e
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-29 10:39:48 +0000

    basic component for play/pause rendering

---
+++ src/components/PlayPause.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class PlayPause extends Component {
+
+  render() {
+    return (
+      <div>
+        <button>PlayPause</button>
+      </div>
+    )
+  }
+}
+
+
+export default PlayPause;
---
+++ src/containers/MetronomeContainer.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import PlayPause from '../components/PlayPause'
+
+class MetronomeContainer extends Component {
+
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Metronome</h1>
+        <PlayPause/>
+      </div>
+    )
+  }
+}
+
+
+export default MetronomeContainer;
